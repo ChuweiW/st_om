076e3964adbed852ec1ded14deaa9f76578fba52
Chuwei Wang <118720572+ChuweiW@users.noreply.github.com>
2024-03-06 20:26:19 +0800

    Update recomp.jl

--- case1/recomp.jl
+++ case1/recomp.jl
@@ -4,46 +4,31 @@ include("forward.jl")
 # predicted belief state
 function Fhat(as::augmented_state,t::Int)
     temp_as=[];
-    if (as.I1 == 1)|| (as.I2==1)
-        if as.I1==1 && as.I2==1
-            if intersection(as.mathx,convert(HPolytope,intersection(goal[1],goal[2])))!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,convert(HPolytope,intersection(goal[1],goal[2]))),0,0,1));
-            end
-            if intersection(as.mathx,goal[1]\goal[2])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[1]\goal[2]),0,1,1));
-            end
-            if intersection(as.mathx,goal[2]\goal[1])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[2]\goal[1]),1,0,1));
-            end
-            if intersection(as.mathx,X_goal12)!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,X_goal12),1,1,1));
-            end
-        elseif as.I1== 0 && as.I2==1
-            if intersection(as.mathx,goal[2])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[2]),0,0,1));
-            end
-            if intersection(as.mathx,X\goal[2])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,X\goal[2]),0,1,1));
-            end
-        elseif as.I1== 1 && as.I2==0
+    if t<=endt[1]
+        if (as.I1 == 1)
             if intersection(as.mathx,goal[1])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[1]),0,0,1));
+                push!(temp_as,augmented_state(intersection(as.mathx,goal[1]),0,1));
             end
-            if intersection(as.mathx,X\goal[1])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,X\goal[1]),1,0,1));
+            if intersection(as.mathx,goal2l1)!= LazySets.∅(2)
+                push!(temp_as,augmented_state(intersection(as.mathx,goal2l1),1,1));
             end
+            if intersection(as.mathx,goal2r1)!= LazySets.∅(2)
+                push!(temp_as,augmented_state(intersection(as.mathx,goal2r1),1,1));
+            end
+        else
+            push!(temp_as,as);
         end
     end
-    if (t<begint[3])&& (as.I1+as.I2==0)
+    if (t<begint[2])&& (t>endt[1])
         push!(temp_as,as);
     end
-    if t>=begint[3]
+    if t>=begint[2]
         if (as.I3 == 1)
             if intersection(as.mathx,goal[4])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[4]),0,0,0));
+                push!(temp_as,augmented_state(intersection(as.mathx,goal[4]),0,0));
             end
             if intersection(as.mathx,goal[3]\goal[4])!= LazySets.∅(2)
-                push!(temp_as,augmented_state(intersection(as.mathx,goal[3]\goal[4]),0,0,1));
+                push!(temp_as,augmented_state(intersection(as.mathx,goal[3]\goal[4]),0,1));
             end
         else
             push!(temp_as,as);
@@ -52,41 +37,29 @@ function Fhat(as::augmented_state,t::Int)
     return temp_as;
 end
 
-# Trigger-time
-function determine_k(t,I1,I2,I3)
-    step = 1; k = 1; unsafeflag = false;
-    Forward_set = forwardset1(z[t],vz[t]);
-    fs = feasible_set(t+1,I1,I2,I3)
-    Augset = []; 
-    for i in 1:length(Fhat(augmented_state(Forward_set,I1,I2,I3),t+1))
-        push!(Augset,Fhat(augmented_state(Forward_set,I1,I2,I3),t+1)[i]); # t+1
-    end
-    if issubset(Forward_set,fs)==false
-        return 1,I1,I2,I3;
-    end
-    while(step<10)
-        if length(get_Ia(Augset))==0
-            k = step;
-            I1 = Augset[1].I1;
-            I2 = Augset[1].I2;
-            I3 = Augset[1].I3;
+function safecheck(Augset, fs)
+    for i in 1:length(Augset)
+        if issubset(Augset[i].mathx,fs)==false
+            return false
+        else 
+            return true
         end
-        Augset_temp = [];
-        while(length(Augset)>0)
-            Augstate = pop!(Augset)
-            if (forwardset(Augstate.mathx) ⊆ feasible_set(t+step+1,Augstate.I1,Augstate.I2,Augstate.I3))==false
-                unsafeflag = true;
-                break;
-            end
-            for i in 1:length(Fhat(augmented_state(forwardset(Augstate.mathx),Augstate.I1,Augstate.I2,Augstate.I3),t+step+1))
-                push!(Augset_temp,Fhat(augmented_state(forwardset(Augstate.mathx),Augstate.I1,Augstate.I2,Augstate.I3),t+step+1)[i]);
+    end
+end
+
+function determinecheck(augset)
+    for i in 1:length(augset)-1
+        for j in i+1:length(augset)
+            intersect= intersection(augset[i].mathx, augset[j].mathx)
+            if intersect != LazySets.∅(2) 
+                if issubset(intersect,Border[1]) || issubset(intersect,Border[2])
+                    continue
+                end
+                if (augset[i].I1 != augset[j].I1) || (augset[i].I3 != augset[j].I3)
+                    return false
+                end
             end
         end
-        if unsafeflag == true
-            break;
-        end
-        Augset = Augset_temp;
-        step += 1;
     end
-    return k+1,I1,I2,I3;
+    return true
 end
